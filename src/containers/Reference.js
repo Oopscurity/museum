6a97a3b158dbf280f42bab2e91ccdb77c0922e70
Oopscurity <headlessfirst@gmail.com>
2016-09-25 14:13:31 +0300

    Reference containerg

---
+++ src/containers/Reference.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
+import Markdown from 'react-markdown';
+import { Map as ImmutableMap } from 'immutable';
+
+import { createSelector } from '../selectors';
+import ModalContainer from './Modal';
+
+class Reference extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps !== this.props;
+  }
+
+  render() {
+    if (!this.props.title) {
+      return false;
+    }
+
+    return (
+      <ModalContainer>
+        <div>
+          <Helmet title={this.props.title} />
+          <Markdown source={this.props.description} />
+        </div>
+      </ModalContainer>
+    );
+  }
+}
+
+const referenceSelector = createSelector(
+  state => state.getIn(['visualization', 'nodes']),
+  state => state.getIn(['visualization', 'referenceId']),
+  (nodes, referenceId) => (nodes.getIn([referenceId, 'data'], ImmutableMap({})))
+);
+
+const inputSelector = createSelector(
+  [referenceSelector],
+  reference => ({
+    description: reference.get('description'),
+    title: reference.get('title')
+  })
+);
+
+export default connect(inputSelector)(Reference);

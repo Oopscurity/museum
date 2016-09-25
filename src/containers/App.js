8b7fa4ae2ba55ca9ad33fdf223b865197d47ed90
Oopscurity <headlessfirst@gmail.com>
2016-09-25 12:21:36 +0300

    Cleaned page's default margin and padding

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     const { children } = this.props;
 
     return (
-      <div>
+      <div className="page">
         {children}
       </div>
     );
